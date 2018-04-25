3a5abaa7301446562578f87f41668adc59de9a0c
PKullberg <peter.kullberg@helsinki.fi>
2018-04-25 12:46:28 +0300

    corrected small typos in negexp_smooth.R

--- negexp_smooth.R
+++ negexp_smooth.R
@@ -1,4 +1,4 @@
-# rasters smoothing with negative exponential function 
+# raster smoothing with negative exponential function 
 library(raster)
 
 # test landscape
@@ -22,22 +22,22 @@ cell_nex_smooth <- function(ras, cell_n, alpha) {
   }
 
 # create smoothed raster (This is rather bad way for working with rasters, but I just wanted to be quick)
-smoothed <- tl2
-smoothed[] <- sapply(seq(ncell(tl2)), function(x) cell_nex_smooth(tl2, x, 0.0001))
+smoothed <- tl
+smoothed[] <- sapply(seq(ncell(tl)), function(x) cell_nex_smooth(tl, x, 0.0001))
 
 # plot the results
 par(mfrow = c(1,2))
-plot(tl2)
+plot(tl)
 plot(smoothed)
 
 
 # Additionallly create summary statistics for indiwidual patches
-clumps <- clump(tl2, directions = 4)
+clumps <- clump(tl, directions = 4)
 new_vals <- zonal(smoothed, clumps, sum)
 cons <- reclassify(clumps, new_vals)
 
 par(mfrow = c(2,2))
-plot(tl2)
+plot(tl)
 plot(smoothed/maxValue(smoothed))
 plot(cons)
 
