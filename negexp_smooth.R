a1c3bb439f272d3c80a5b45adbf8353668ee2588
PKullberg <peter.kullberg@helsinki.fi>
2018-05-01 20:05:12 +0300

    small modifications to plotting

--- negexp_smooth.R
+++ negexp_smooth.R
@@ -1,14 +1,24 @@
 # raster smoothing with negative exponential function 
 library(raster)
+library(rasterVis)
+library(RColorBrewer)
 
 # test landscape
 par(mfrow = c(1,1))
 tl <- raster(matrix(NA, 10, 10))
-tl[1:2, 1:2] <- 1
+tl[1:2, 1:2] <- 1.2
 tl[8:9, 8:9] <- 1
-tl[7:6, 7:6] <- 1
-tl[9, 1:4] <- 1
-plot(tl)
+tl[7:6, 7:6] <- 2
+tl[9, 1:4] <- 1.2
+tl <- tl / 2
+plot(tl, col = brewer.pal(3, "Blues"))
+
+# smoother visualization
+## landscape use 
+alpha = 2/1
+plot(seq(0,6, length.out = 100), exp(-alpha * seq(0,6, length.out = 100)), type = "l", xlab = "distance (km)", ylab = "value")
+abline(h = exp(-alpha), lty = 2)
+abline(v = 1, lty = 3)
 
 # smoother fucntion for a cell. Takes in raster (ras), cel number (cell_n), and alpha parameter that scales landscape use
 cell_nex_smooth <- function(ras, cell_n, alpha) {
@@ -23,22 +33,17 @@ cell_nex_smooth <- function(ras, cell_n, alpha) {
 
 # create smoothed raster (This is rather bad way for working with rasters, but I just wanted to be quick)
 smoothed <- tl
-smoothed[] <- sapply(seq(ncell(tl)), function(x) cell_nex_smooth(tl, x, 0.0001))
+smoothed[] <- sapply(seq(ncell(tl)), function(x) cell_nex_smooth(tl, x, 0.00007))
+smoothed <- smoothed / maxValue(smoothed)
 
-# plot the results
-par(mfrow = c(1,2))
-plot(tl)
-plot(smoothed)
-
-
-# Additionallly create summary statistics for indiwidual patches
+# Additionally create summary statistics for individual patches
+just_smooth <- mask(smoothed, tl)
 clumps <- clump(tl, directions = 4)
-new_vals <- zonal(smoothed, clumps, sum)
+new_vals <- zonal(smoothed, clumps, mean)
 cons <- reclassify(clumps, new_vals)
 
-par(mfrow = c(2,2))
-plot(tl)
-plot(smoothed/maxValue(smoothed))
-plot(cons)
-
+vis_block <- stack(tl, smoothed, just_smooth, cons)
+redTheme10 <- rasterTheme(region = brewer.pal(9, "Blues"))
+levelplot(vis_block, par.settings = redTheme10, names.attr = c("test landscape", "smoothed landscape", "smoothed patches", "aggreagated patches"))
+new_vals
 
